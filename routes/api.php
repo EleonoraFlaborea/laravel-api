8cfe7eca37d27791701dd3717a123e7ab088087d
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-29 10:26:52 +0100

    add api test, add controller

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+
+Route::get('/projects', function () {
+    $projects = Project::all();
+    return response()->json('projects');
 });
