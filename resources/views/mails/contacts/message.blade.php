d86b64bcbb184588322d07a6651fa33adb824bfb
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-04-07 11:59:23 +0200

    Add controller, and message.blade

---
+++ resources/views/mails/contacts/message.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boolfolio</title>
+
+    <style>
+      body{
+        background-color: lightgrey;
+        text-align: center;
+        font-family: Verdana, sans-serif;
+      }
+    </style>
+    
+</head>
+<body>
+    <p>è stato inviato un messaggio</p>
+</body>
+</html>
